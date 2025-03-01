cead013591131a521fa946215800e9a2c27be58a
EC2 Default User <ec2-user@ip-10-32-1-219.ec2.internal>
2025-03-01 21:49:40 +0000

    fix build layer script and add entrypoint script

--- etl-lambda/build-layer.sh
+++ etl-lambda/build-layer.sh
@@ -1,4 +1,26 @@
+#!/bin/bash
 set -eo pipefail
-rm -rf package
-cd my_function
-pip3 install --target ../package/python -r requirements.txt
+
+# Clean up previous builds
+rm -rf package/
+mkdir -p package/python/lib/python3.11/site-packages
+
+# Install dependencies into the layer directory
+pip3 install --platform manylinux2014_x86_64 \
+    --target=package/python/lib/python3.11/site-packages \
+    --implementation cp \
+    --python-version 3.11 \
+    --only-binary=:all: --upgrade \
+    pandas==2.0.3 \
+    numpy==1.24.3 \
+    python-dotenv==1.0.0 \
+    sqlalchemy==2.0.20 \
+    psycopg2-binary==2.9.7 \
+    boto3==1.28.0
+
+# Remove unnecessary files to reduce layer size
+cd package/python/lib/python3.11/site-packages
+find . -type d -name "tests" -exec rm -rf {} +
+find . -type d -name "__pycache__" -exec rm -rf {} +
+rm -rf *.dist-info
+rm -rf *.egg-info
